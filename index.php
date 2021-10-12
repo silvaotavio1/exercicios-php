4ee2fe0ad59bb35345267a444c1e71bc068b6907
silvaotavio1 <otaviosilva96@gmail.com>
2021-10-12 19:20:36 -0300

    Ajuste html

--- index.php
+++ index.php
@@ -1,26 +1,29 @@
 <!DOCTYPE HTML>
 <html lang="pt-br">
-<head>
-  <meta charset="UTF-8">
-    <title>Menu Dropdown Horizontal - Linha de Código</title>
-     <!-- Aqui chamamos o nosso arquivo css externo -->
-    <link rel="stylesheet" type="text/css"  href="estilo.css" />
-</head>
-<body>
-<nav>
-  <ul class="menu">
 
-  <li><a href="#">Exemplos</a>
-  <?php
-        for($k=1;$k<=7;$k++)
-        {
-            echo '<ul><li><a href="ex' . $k . '.php">Ex ' . $k . '</a></li></ul>';
-        }
-  ?>
-  </li>
-<li><a href="#">Reposiorio</a></li>
-		<!-- <li><a href="#">Ex 1</a></li> -->
-	  		<!-- 
+<head>
+    <meta charset="UTF-8">
+    <title>Menu Dropdown Horizontal - Linha de Código</title>
+    <!-- Aqui chamamos o nosso arquivo css externo -->
+    <link rel="stylesheet" type="text/css" href="estilo.css" />
+</head>
+
+<body>
+    <nav>
+        <ul class="menu">
+
+            <ul>
+                <li><a href="#">Exemplos</a>
+                    <?php
+                    for ($k = 1; $k <= 7; $k++) {
+                        echo '<li><a href="ex' . $k . '.php">Ex ' . $k . '</a></li>';
+                    }
+                    ?>
+            </ul>
+            </li>
+            <li><a href="#">Reposiorio</a></li>
+            <!-- <li><a href="#">Ex 1</a></li> -->
+            <!-- 
 	         	<ul>
 	                  <li><a href="#">Web Design</a></li>
 	                  <li><a href="#">SEO</a></li>
@@ -28,7 +31,8 @@
 	       		</ul>
 			</li> -->
 
-</ul>
-</nav>
+        </ul>
+    </nav>
 </body>
+
 </html>
