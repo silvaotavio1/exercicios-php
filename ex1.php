0d5f1ed1583b8b808c03482e004840aceb22b0b1
silvaotavio1 <otaviosilva96@gmail.com>
2021-10-12 21:25:56 -0300

    no message

--- ex1.php
+++ ex1.php
@@ -1,9 +1,6 @@
 <form action="ex1.php" method="post">
     <p>Número de posições no array: <input value="<?php echo $_POST['numPosicoes']; ?>" type="number" name="numPosicoes" /></p>
-    <p>Numero de inversões: <input value="
-    <?php echo
-    $_POST['numInversoes'] > $_POST['numPosicoes'] ? $_POST['numPosicoes'] : $_POST['numInversoes'];
-    ?>" type="number" name="numInversoes" /></p>
+    <p>Numero de inversões: <input value="<?php echo $_POST['numInversoes'] > $_POST['numPosicoes'] ? $_POST['numPosicoes'] : $_POST['numInversoes'];?>" type="number" name="numInversoes" /></p>
     <p><input type="submit" /></p>
 </form>
 
