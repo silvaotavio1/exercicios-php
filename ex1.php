a008640ea822c58bfaff7199f53f11f85179816a
silvaotavio1 <otaviosilva96@gmail.com>
2021-10-12 22:31:36 -0300

    no message

--- ex1.php
+++ ex1.php
@@ -15,32 +15,31 @@ printArray(inverterArray());
 function defineInversao()
 {
     $retorno = $_POST['numInversoes'] > $_POST['numPosicoes'] ? $_POST['numPosicoes'] : $_POST['numInversoes'];
-    return ($retorno < 1? 0: $retorno);
+    return ($retorno < 1 ? 0 : $retorno);
 }
 
 function definePosicoes()
 {
     $retorno = $_POST['numPosicoes'];
-    return ($retorno < 1? 0: $retorno);
+    return ($retorno < 1 ? 0 : $retorno);
 }
 
 function criarArray()
 {
     $retorno = array();
 
-    for($k=0; $k<definePosicoes(); $k++)
-    {
-        $retorno[$k] = $k+1;
+    for ($k = 0; $k < definePosicoes(); $k++) {
+        $retorno[$k] = $k + 1;
     }
 
     return $retorno;
 }
 
-function printArray($array){
+function printArray($array)
+{
     echo "<b>[";
-    for($k=0; $k<sizeof($array); $k++)
-    {
-        echo $array[$k] . ((($k+1)==sizeof($array))? "": ",");
+    for ($k = 0; $k < sizeof($array); $k++) {
+        echo $array[$k] . ((($k + 1) == sizeof($array)) ? "" : ",");
     }
     echo "]</b>";
 }
@@ -49,16 +48,17 @@ function inverterArray()
 {
     $array = criarArray();
     $num_trocas = defineInversao();
-    $num_elementos = definePosicoes();//poderia pegar o sizeof($array)
+    $num_elementos = definePosicoes(); //poderia pegar o sizeof($array)
 
-    $distancia = $num_elementos - $num_trocas;
+    $distancia = $num_elementos - 1;
 
-    $aux = $array[0];
-    for($k=0; $k<$distancia; $k++)
-    {
-        $array[$k] = $array[$k+1];
+    for ($i = 0; $i < $num_trocas; $i++) {
+        $aux = $array[0];
+        for ($k = 0; $k < $distancia; $k++) {
+            $array[$k] = $array[$k + 1];
+        }
+        $array[$k] = $aux;
     }
-    $array[$k] = $aux;
 
     return $array;
 }
