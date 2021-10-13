7bab8fcc4c9745dd9d3b02b1aff4e71dca5a2ac1
silvaotavio1 <otaviosilva96@gmail.com>
2021-10-12 21:31:58 -0300

    no message

--- ex1.php
+++ ex1.php
@@ -1,5 +1,5 @@
 <form action="ex1.php" method="post">
-    <p>Número de posições no array: <input value="<?php echo $_POST['numPosicoes']; ?>" type="number" name="numPosicoes" /></p>
+    <p>Número de posições no array: <input value="<?php echo definePosicoes(); ?>" type="number" name="numPosicoes" /></p>
     <p>Numero de inversões: <input value="<?php echo defineInversao() ?>" type="number" name="numInversoes" /></p>
     <p><input type="submit" /></p>
 </form>
@@ -12,6 +12,12 @@ print_r($_POST);
 function defineInversao()
 {
     $retorno = $_POST['numInversoes'] > $_POST['numPosicoes'] ? $_POST['numPosicoes'] : $_POST['numInversoes'];
+    return ($retorno < 1? 0: $retorno);
+}
+
+function definePosicoes()
+{
+    $retorno = $_POST['numPosicoes'];
     return ($retorno < 1? 0: $retorno);
 }
 
