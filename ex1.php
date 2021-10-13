5ef324e52288f41871331e41ccce78c868243406
silvaotavio1 <otaviosilva96@gmail.com>
2021-10-12 21:22:02 -0300

    iniciando ex1

--- ex1.php
+++ ex1.php
@@ -1,6 +1,6 @@
 <form action="ex1.php" method="post">
- <p>Número de posições no array: <input type="text" name="name" /></p>
- <p>Numero de inversões: <input type="text" name="age" /></p>
+ <p>Número de posições no array: <input value="<?php echo $_POST['numPosicoes'];?>" type="number" name="numPosicoes" /></p>
+ <p>Numero de inversões: <input value="<?php echo $_POST['numPosicoes'];?>" type="number" name="numInversoes" /></p>
  <p><input type="submit" /></p>
 </form>
 
@@ -8,5 +8,7 @@
 
 echo "<br><pre>";
 print_r($_POST);
+
+
 
 ?>
