b71d2e73761788626632b95b9cf60433ecaafae2
silvaotavio1 <otaviosilva96@gmail.com>
2021-10-13 22:09:28 -0300

    no message

--- ex4.php
+++ ex4.php
@@ -10,7 +10,7 @@ include("inc/funcoes.php");
     <title>Exercicio 4</title>
 </head>
 
-<form action="ex1.php" method="post">
+<form action="ex4.php" method="post">
     <p>Número de posições no array: <input value="<?php echo definePosicoes(); ?>" type="number" name="numPosicoes" /></p>
     <p>Numero de inversões: <input value="<?php echo defineInversao() ?>" type="number" name="numInversoes" /></p>
     <p>
