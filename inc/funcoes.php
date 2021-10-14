603c4cb3292f281b5a0cf287832fae00eebaafd1
silvaotavio1 <otaviosilva96@gmail.com>
2021-10-13 23:37:25 -0300

    no message

--- inc/funcoes.php
+++ inc/funcoes.php
@@ -64,87 +64,33 @@ function criarArrayR()
     return $retorno;
 }
 
-function ordenaArray($array)
+function quicksort(&$array, $esquerda, $direita)
 {
-    $tam = sizeof($array);
-
-    if ($tam < 2) {
-        return $array;
-    }
-
-    $pivo = $array[0];
-    $aux = $pivo; //deixamos livre para troca da posicao zero
-    $e = 0; //pergunta se a direita eh maior // se sim vai para a pos vazia e passa a bola // se não $e++ 
-    $d = $tam - 1; //Pergunta se o numero eh menor q o pivo // Se sim vai para a pos vazia e passa a bola // se não $d -- 
-
-    $bola = 'd';
-
-    $cont = 0;
-
-    while ($e <> $d) {
-        if ($bola == 'd') {
-            if ($array[$d] < $pivo) {
-                $array[$e] = $array[$d];
-                $bola = 'e';
-                $e++;
-            } else {
-                $d--;
-            }
-        } elseif ($bola == 'e') {
-            if ($array[$e] > $pivo) {
-                $array[$d] = $array[$e];
-                $bola = 'd';
-                $d--;
-            } else {
-                $e++;
-            }
-        }
-        $cont++;
-
-        if ($cont > 5000)
-            exit('erro');
-    }
-
-    echo "<br>$cont<br>";
-
-    $array[$e] = $aux;
-
-    return $array;
-}
-
-// function for quick sort which calls partition function 
-// to arrange and split the list based on pivot element
-// quicksort is a recursive function
-function quicksort(&$Array, $left, $right)
-{
-    if ($left < $right) {
-        $pivot = partition($Array, $left, $right);
-        quicksort($Array, $left, $pivot - 1);
-        quicksort($Array, $pivot + 1, $right);
+    if ($esquerda < $direita) 
+    {
+        $pivo = divisoria($array, $esquerda, $direita);
+        quicksort($array, $esquerda, $pivo - 1);
+        quicksort($array, $pivo + 1, $direita);
     }
 }
 
-// partition function arrange and split the list 
-// into two list based on pivot element
-// In this example, last element of list is chosen 
-// as pivot element. one list contains all elements 
-// less than the pivot element another list contains 
-// all elements greater than the pivot element
-function partition(&$Array, $left, $right)
+function divisoria(&$array, $esquerda, $direita)
 {
-    $i = $left;
-    $pivot = $Array[$right];
-    for ($j = $left; $j <= $right; $j++) {
-        if ($Array[$j] < $pivot) {
-            $temp = $Array[$i];
-            $Array[$i] = $Array[$j];
-            $Array[$j] = $temp;
+    $i = $esquerda;
+    $pivo = $array[$direita];
+    for ($j = $esquerda; $j <= $direita; $j++) 
+    {
+        if ($array[$j] < $pivo) 
+        {
+            $temp = $array[$i];
+            $array[$i] = $array[$j];
+            $array[$j] = $temp;
             $i++;
         }
     }
 
-    $temp = $Array[$right];
-    $Array[$right] = $Array[$i];
-    $Array[$i] = $temp;
+    $temp = $array[$direita];
+    $array[$direita] = $array[$i];
+    $array[$i] = $temp;
     return $i;
 }
