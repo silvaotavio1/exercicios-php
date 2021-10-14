4497e4152e40047c3a1bb84e2d429c8dd189fc61
silvaotavio1 <otaviosilva96@gmail.com>
2021-10-13 23:38:30 -0300

    no message

--- ex1.php
+++ ex1.php
@@ -12,7 +12,6 @@ include("inc/funcoes.php");
 
 <form action="ex1.php" method="post">
     <p>Número de posições no array: <input value="<?php echo definePosicoes(); ?>" type="number" name="numPosicoes" /></p>
-    <p>Numero de inversões: <input value="<?php echo defineInversao() ?>" type="number" name="numInversoes" /></p>
     <p>
         <button type="submit">Executar</button>
         <button type="button" onclick="window.open('/testephp/', '_self')">Voltar</button>
