3cf12d0a83f6e32b83eb1a77776df8d0971355ca
silvaotavio1 <otaviosilva96@gmail.com>
2021-10-14 00:06:21 -0300

    no message

--- ex2.php
+++ ex2.php
@@ -25,10 +25,42 @@ include("inc/funcoes.php");
 $array = criarArrayR();
 
 echo "<br>Array antes de sofrer alteração: ";
-printArray($array);//criando array aleatorio para ordernar
+printArray($array); //criando array aleatorio para ordernar
 
 echo "<br>Array após sofrer alteração: ";
-quicksort($array, 0, sizeof($array) - 1, "C");
+
+$cont_pares = 0;
+$cont_impares = 0;
+$pares = $array();
+$impares = $array();
+
+for ($k = 0; $k < sizeof($array); $k++) 
+{
+    if ($array[$k] % 2 == 0) 
+    {
+        $pares[$cont_pares] = $array[$k];
+        $cont_pares++;
+    } 
+    else 
+    {
+        $impares[$cont_impares] = $array[$k];
+        $cont_impares++;
+    }
+}
+
+quicksort($pares, 0, sizeof($pares) - 1, "C");
+quicksort($impares, 0, sizeof($impares) - 1, "D");
+
+$k;
+for ($k = 0; $k < sizeof($pares); $k++) 
+{
+    $array[$k] = $pares[$k];
+}
+for ($k = $k; $k<sizeof($pares) + sizeof($impares);$k++)
+{
+    $array[$k] = $impares[$k - sizeof($pares)];
+}
+
 printArray($array);
 
 ?>
