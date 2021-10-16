8fc677e43c5e7f97b871f12cced8787b745616cb
silvaotavio1 <otaviosilva96@gmail.com>
2021-10-15 22:24:22 -0300

    no message

--- ex4.php
+++ ex4.php
@@ -31,9 +31,6 @@ echo "<br>Numero de possibilidades: <b>[";
 echo numeroCombinacoes(sizeof($array)) . "]</b><br>";
 
 echo "<br>Exemplo de possibilidades: <b>";
-// triangulos($array);
-
-combinacoes($array);
-echo $texto;
+printArrayL(combinacoes($array));
 
 ?>
